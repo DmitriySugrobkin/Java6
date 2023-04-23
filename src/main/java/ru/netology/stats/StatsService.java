91aeb874e29ab51cbeb71f7383808530004c8602
Sugrobkin <dsugrobkin148@gmail.com>
2023-04-23 18:42:50 +0300

    First commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,63 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / sales.length;
+    }
+
+    public int maxMonth(long[] sales) {
+        int monthMax = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[monthMax]) {
+                monthMax = i;
+            }
+        }
+        return monthMax + 1;
+    }
+
+    public int minMonth(long[] sales) {
+        int monthMin = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[monthMin]) {
+                monthMin = i;
+            }
+        }
+        return monthMin + 1;
+    }
+
+    public int monthsBelowAverage(long[] sales) {
+        int counter = 0;
+        long averageSale = average(sales);
+
+        for (long sale : sales) {
+            if (sale < averageSale) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int monthsAboveAverage(long[] sales) {
+        int counter = 0;
+        long averageSale = average(sales);
+
+        for (long sale : sales) {
+            if (sale > averageSale) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+
+}
